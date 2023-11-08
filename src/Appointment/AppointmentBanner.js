aeacd08ed45532dadf1f190db9a40a0f4f382f28
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-11-08 12:07:07 +0600

    react datePicker, appointment, appointmentBanner: package added, component added, created appointement bannar successfully.

---
+++ src/Appointment/AppointmentBanner.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import chair from "../assets/images/chair.png";
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import { format } from 'date-fns';
+
+const AppointmentBanner = () => {
+
+    const [date, setDate] = useState(new Date());
+
+    let footer = <p>Please pick a day.</p>;
+    if (date) {
+        footer = <p>You picked {format(date, 'PP')}.</p>;
+    };
+
+
+    return (
+        <section className="hero min-h-screen ">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="Dentist chair" />
+                <div>
+                    <DayPicker mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                        footer={footer}></DayPicker>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AppointmentBanner;
