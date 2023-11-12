7479784269d5fbbee56b29ac7238d42a42fcaf39
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-11-12 19:31:04 +0600

    dashbaord: daisy drawer added and connect with navbar.

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -5,15 +5,14 @@ const Dashboard = () => {
     return (
         <section>
             <div className="drawer lg:drawer-open">
-                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col items-center justify-center">
-                    <h2 className="text-5xl text-purple-500">Dashboard</h2>
+                <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content">
+                    <h2 className="text-3xl font-bold text-purple-500"> Welcome to your Dashboard.</h2>
                     <Outlet></Outlet>
-                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
                 </div>
                 <div className="drawer-side">
-                    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <label htmlFor="dashboard-sidebar" aria-label="close sidebar" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
                         {/* Sidebar content here */}
                         <li><Link to="/dashboard">Sidebar Item 1</Link></li>
