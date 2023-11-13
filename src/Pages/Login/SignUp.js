2c3b24e0b4e52f271ce0f516c09c489127165dd4
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-11-13 20:57:57 +0600

    useToken: added for saving data in database.

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -4,6 +4,7 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form"
 import Loading from '../Shared/Loading';
 import { Link, useNavigate } from 'react-router-dom';
+import useToken from '../../hooks/useToken';
 
 
 const SignUp = () => {
@@ -22,6 +23,8 @@ const SignUp = () => {
     ] = useCreateUserWithEmailAndPassword(auth);
     const [updateProfile, updating, updateError] = useUpdateProfile(auth);
 
+    const [token] = useToken(user || gUser);
+
     const navigate = useNavigate();
 
     let signInError;
@@ -36,8 +39,10 @@ const SignUp = () => {
         signInError = <p className="text-red-500"><small>{error?.message || gError?.message || updateError?.message}</small></p>
     };
 
-    if (gUser) {
-        console.log(user || gUser);
+    if (token) {
+        // console.log(user || gUser);
+        navigate("/appointment");
+
     };
 
     const onSubmit = async (data) => {
@@ -45,7 +50,6 @@ const SignUp = () => {
         await createUserWithEmailAndPassword(data.email, data.password);
         await updateProfile({ displayName: data.name });
         // console.log("update done");
-        navigate("/appointment");
     };
 
 
